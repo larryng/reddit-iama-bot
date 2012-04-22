901171e342fdd3b4f90faa6cdbf2fc60d9031c28
Larry Ng <ng.larry@gmail.com>
2012-04-21 21:06:53 -0400

    min_comments

--- go.py
+++ go.py
@@ -13,7 +13,7 @@ USERNAME = os.environ['REDDIT_USERNAME']
 PASSWORD = os.environ['REDDIT_PASSWORD']
 MONGO_URI = os.environ['MONGOLAB_URI']
 DB_NAME = urlparse.urlparse(MONGO_URI).path.strip('/')
-MIN_COMMENTS = os.environ.get('IAMA_MIN_COMMENTS', 200)
+MIN_COMMENTS = int(os.environ.get('IAMA_MIN_COMMENTS', 200))
 WAIT_TIME = float(os.environ.get('IAMA_WAIT_TIME', 60.0))
 
 BASE_URL = u'http://www.reddit.com/'
