e7d0dc8bfa75b50a99c377df502345f327c998ed
Larry Ng <ng.larry@gmail.com>
2012-05-09 21:26:45 -0400

    all_comments and deep_count (currently unused, though)

--- iama/utils.py
+++ iama/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+import narwal
 import pymongo
 
 from envvars import MONGO_URI, DB_NAME
@@ -17,3 +18,21 @@ def log(s):
 def quotify(s):
   """reddit markdown quotes a string"""
   return u'> {}'.format(s.replace('\n', '\n> '))
+
+
+def all_comments(clisting):
+  while clisting.has_more:
+    clisting.children = clisting[:-1] + list(clisting.more())
+  for c in clisting.children:
+    if c.replies:
+      all_comments(c.replies)
+
+
+def deep_count(comments):
+  count = 0
+  for c in comments:
+    if isinstance(c, narwal.things.Comment):
+      count += 1
+      if c.replies:
+        count += deep_count(c.replies)
+  return count
